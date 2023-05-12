f20333076f62b3398b075584f81b5fa892e12d8a
tjp2518 <jptian@whut.edu.cn>
2023-05-12 15:38:53 +0800

    done with tjp

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -12,29 +12,32 @@ public class TestArrayDequeEC {
         ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
         Integer a;
         Integer b;
+        StringBuilder returnMsg = new StringBuilder("\n");
 
-        for (int i = 0; i < 1000; i += 1) {
+        for (int i = 0; i < 1001; i += 1) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
-
             if (numberBetweenZeroAndOne < 0.25) {
                 sad1.addLast(i);
                 ads1.addLast(i);
+                returnMsg.append("addLast(").append(i).append(")\n");
             } else if (numberBetweenZeroAndOne < 0.5) {
                 sad1.addFirst(i);
                 ads1.addFirst(i);
+                returnMsg.append("addFirst(").append(i).append(")\n");
+
             } else if (numberBetweenZeroAndOne < 0.75) {
                 if (sad1.size() > 0) {
                     a = sad1.removeLast();
                     b = ads1.removeLast();
-                    assertEquals("The last call to the removeFirst method, " +
-                            "which should be return " + b + " but return " + a, b, a);
+                    returnMsg.append("removeLast(").append(")\n");
+                    assertEquals(returnMsg.toString(), b, a);
                 }
             } else {
-                if ( sad1.size() > 0){
+                if (sad1.size() > 0) {
                     a = sad1.removeFirst();
                     b = ads1.removeFirst();
-                    assertEquals("The last call to the removeLast method, " +
-                            "which should be return " + b + " but return " + a, b, a);
+                    returnMsg.append("removeFirst(").append(")\n");
+                    assertEquals(returnMsg.toString(), b, a);
                 }
             }
         }
